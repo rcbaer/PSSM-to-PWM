a8e81797e37aacbba045a7d645a717354dbaf2a6
rcbaer <rcbaer@gmail.com>
2020-05-25 12:22:29 -0400

    Update PSSM-to-PWM.py
    changed variable names to better reflect what they are.
    added bit of code to generate text files

--- PSSM-to-PWM.py
+++ PSSM-to-PWM.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Script to convert a Position specific scoring matrices (PSSMs) file
+from RegulonDB to the minimal MEME format with position weight matrix
+(PWM) data.
 """
 import re
-import numpy
 import pandas as pd
 
-# Open PWM file downloaded from RegulonDB
-pwm_file = open('pwm.txt', mode = 'r')
-pwm_data = pwm_file.readlines()
+# Open PSSM file downloaded from RegulonDB
+PSSM_file = open('pwm.txt', mode = 'r')
+PSSM_data = PSSM_file.readlines()
+PSSM_file.close()
 
 # Splits list into list of lists, each TF getting its own list
-split_pwms = []
-for item in pwm_data:
-    if re.match("Transcription Factor ID", item) or len(split_pwms) == 0:
-        split_pwms.append([item])
+split_PSSM = []
+for item in PSSM_data:
+    if re.match("Transcription Factor ID", item) or len(split_PSSM) == 0:
+        split_PSSM.append([item])
     else:
-        split_pwms[-1].append(item)
+        split_PSSM[-1].append(item)
         
-split_pwms = split_pwms[1:len(pwm_data)]
+split_PSSM = split_PSSM[1:len(PSSM_data)]
 
 # Gets PCM data from each TF sublist as panda dataframe
 pcm_data = []
-for item in split_pwms:
+for item in split_PSSM:
     cur_pcm_data = item[-7:-3]
     cur_pcm_lol = []
     for line in cur_pcm_data:
@@ -46,3 +46,16 @@ for item in pcm_copy:
     item['T'] = item['T']/item['counts']
     item['G'] = item['G']/item['counts']
     pwm_data.append(item[['A', 'C', 'G', 'T']])
+    
+# Generates MEME compatable file from PWM
+header_text = 'MEME version 4\n\nALPHABET= ACGT\n\nstrands: + -\n\n'
+string_out = header_text
+for i in range(0,len(split_PSSM)):
+    TF_name = split_PSSM[i][1].split(':')[1]
+    cur_pwm = pwm_data[i].to_string(index = False, header= False)
+    string_out = string_out + 'MOTIF' + TF_name + 'letter-probability matrix:' + '\n' + cur_pwm + '\n\n'
+    
+
+text_file = open("Minimal_MEME.txt", "w")
+text_file.write(string_out)
+text_file.close()
